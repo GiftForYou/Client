fc791a8cf5c0f330f6c204040eaf683933224513
haryanawisnu <arya.wisnu05@gmail.com>
2017-06-10 01:19:10 +0700

    analyze main topic

--- hackcrome/popup.js
+++ hackcrome/popup.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded',function () {
+function onAnalize(){
   chrome.tabs.getSelected(null,function (tab) {
-
-    chrome.tabs.sendMessage(tab.id, {text: 'start'}, function (w) {
-      document.write(w)
-      // alert(w);
+    var myNewUrl = tab.url+"/likes_all";
+    chrome.tabs.update(tab.id, {url: myNewUrl});
+    chrome.tabs.onUpdated.addListener(function(tabId , info) {
+      if (info.status == "complete") {
+        chrome.tabs.sendMessage(tab.id, {text: 'start'}, function (w) {
+          document.getElementById("result").innerHTML = JSON.stringify(w);
+        });
+      }
     });
+
   })
+}
+
+document.addEventListener('DOMContentLoaded',function () {
+  //Analize -- baru maintopic
+  document.getElementById("analize").addEventListener("click", onAnalize);
+
+  //Rekomendasi--- kirim data result ke server, dapetin barang rekomendasinya
+  //document.getElementById("recomendation").addEventListener("click"................
 })
