b788a79dadf5a327b244c8c191d1333d45bd1f41
haryanawisnu <arya.wisnu05@gmail.com>
2017-06-12 15:58:19 +0700

    add analyze

--- hackcrome/background.js
+++ hackcrome/background.js
@@ -1,4 +1,18 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "http://stackoverflow.com/";
-  chrome.tabs.create({ url: newURL });
-});
+var background = {
+	result:null,
+	init :function() {
+		chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+			if(request.fn in background){
+				background[request.fn](request,sender,sendResponse);
+			}		
+		});
+	},
+	setResult:function(request,sender,sendResponse){
+		this.result=request.data;
+	},
+	getResult:function(request,sender,sendResponse){
+		sendResponse(this.result);
+	}
+}
+
+background.init();
