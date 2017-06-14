60ecded7f7e95c7950a87a58390f70ca93b22d09
zanisis <adzannyaa@gmail.com>
2017-06-14 10:19:36 +0700

    popup edit

--- hackcrome/popup.js
+++ hackcrome/popup.js
@@ -22,20 +22,27 @@ var app = {
           }, 0);
 
           let table = document.createElement("TABLE");
-          table.setAttribute("style", "width: 40%;");
+          table.setAttribute("class", "ui orange celled table");
 
-          let arrays = ["Category","Count"]
+          let arrays = ["Category","Percentase"]
 
+          let tHead = document.createElement("THEAD")
           let tableRow = document.createElement("TR")
           arrays.forEach(function(data){
             let tableHeader = document.createElement("TH")
             tableHeader.appendChild(document.createTextNode(data))
             tableRow.appendChild(tableHeader)
           })
-          table.appendChild(tableRow)
+          tHead.appendChild(tableRow)
+          table.appendChild(tHead)
 
-          w.maintopic.forEach(function(obj){
+          let tBody = document.createElement("TBODY")
+
+          w.maintopic.forEach(function(obj, index){
             let tableRow1 = document.createElement("TR")
+            if(index%2 === 0){
+              index%4 === 0 ? tableRow1.setAttribute("class", "warning") : tableRow1.setAttribute("class", "positive");
+            }
             if(obj.name !== "Semua Suka"){
               let tableDataName = document.createElement("TD")
               tableDataName.appendChild(document.createTextNode(obj.name))
@@ -44,124 +51,106 @@ var app = {
               let persen = Math.floor((obj.count / sum) * 100)
               tableDataCount.appendChild(document.createTextNode(`${persen}%`))
               tableRow1.appendChild(tableDataCount)
-              table.appendChild(tableRow1)
+              tBody.appendChild(tableRow1)
             }
           })
 
-          document.getElementById("myResult").appendChild(table)
+          table.appendChild(tBody)
+          document.getElementById("selfresult").appendChild(table)
+          console.log(app)
           // document.getElementById("result").innerHTML = JSON.stringify(w);
-
-          let dummy = {"maintopic": [
-                    {
-                        "name": "semua suka",
-                        "count": 119
-                    },
-                    {
-                        "name": "film",
-                        "count": 2
-                    },
-                    {
-                        "name": "atlet",
-                        "count": "4"
-                    }
-                ],
-                "detail": [
-                    {
-                        "name": "semua suka",
-                        "data": [
-                            "chealsea",
-                            "nasi goreng",
-                            "john terry fansclub",
-                            "chealsea tangerang",
-                            "chealsea indo",
-                            "fast on farius 8",
-                            "falcon picture"
-                        ]
-                    },
-                    {
-                        "name": "film",
-                        "data": [
-                            "fast on farius 8",
-                            "falcon picture"
-                        ]
-                    },
-                    {
-                        "name": "atlet",
-                        "data": [
-                            "chealsea",
-                            "john terry fansclub",
-                            "chealsea tangerang",
-                            "chealsea indo"
-                        ]
-                    }
-                ]
-            }
-
-          fetch('http://localhost:3000/recomendation', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              data: dummy,
-            })
-          }).then((response)=>{
-            return response.json()
-          }).then(data=>{
-            document.getElementById("showRecomendation").append(JSON.stringify(data.hasil))
-          })
         });
       }
     });
   })
   },
-  addTable : function(){
-    var maintopic=this.result;
-    if(maintopic){
-      var sum = maintopic.reduce(function (initial, data) {
-      let tmp = initial
-      data.name !== "Semua Suka" ? tmp += parseInt(data.count) : null
-      return tmp
-    }, 0);
-
-    let table = document.createElement("TABLE");
-    table.setAttribute("style", "width: 40%;");
-
-    let arrays = ["Category","Count"]
-
-    let tableRow = document.createElement("TR")
-    arrays.forEach(function(data){
-      let tableHeader = document.createElement("TH")
-      tableHeader.appendChild(document.createTextNode(data))
-      tableRow.appendChild(tableHeader)
-    })
-    table.appendChild(tableRow)
-
-    maintopic.forEach(function(obj){
-      let tableRow1 = document.createElement("TR")
-      if(obj.name !== "Semua Suka"){
-        let tableDataName = document.createElement("TD")
-        tableDataName.appendChild(document.createTextNode(obj.name))
-        tableRow1.appendChild(tableDataName)
-        let tableDataCount = document.createElement("TD")
-        let persen = Math.floor((obj.count / sum) * 100)
-        //console.log(persen,'%')
-        tableDataCount.appendChild(document.createTextNode(`${persen}%`))
-        tableRow1.appendChild(tableDataCount)
-        table.appendChild(tableRow1)
+  addRecomend : function(){
+    let dummy = {"maintopic": [
+              {
+                  "name": "semua suka",
+                  "count": 119
+              },
+              {
+                  "name": "film",
+                  "count": 2
+              },
+              {
+                  "name": "atlet",
+                  "count": "4"
+              }
+          ],
+          "detail": [
+              {
+                  "name": "semua suka",
+                  "data": [
+                      "chealsea",
+                      "nasi goreng",
+                      "john terry fansclub",
+                      "chealsea tangerang",
+                      "chealsea indo",
+                      "fast on farius 8",
+                      "falcon picture"
+                  ]
+              },
+              {
+                  "name": "film",
+                  "data": [
+                      "fast on farius 8",
+                      "falcon picture"
+                  ]
+              },
+              {
+                  "name": "atlet",
+                  "data": [
+                      "chealsea",
+                      "john terry fansclub",
+                      "chealsea tangerang",
+                      "chealsea indo"
+                  ]
+              }
+          ]
       }
-    })
 
-    //console.log(table)
-    document.getElementById("myResult").appendChild(table)
-    // document.getElementById("result").innerHTML = JSON.stringify(w);
-    }
-    }
+    fetch('http://localhost:3000/recomendation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data: dummy,
+      })
+    }).then((response)=>{
+      return response.json()
+    }).then((data)=>{
+      console.log(data.hasil)
+      data.hasil.forEach((obj,index)=>{
+        let alpha = document.createElement("a")
+        alpha.setAttribute("class", "teal card")
+        alpha.setAttribute("href", "https://www.bukalapak.com/p/rumah-tangga/home-stuff/z2a6-jual-balmut-chealsea")
+        let divImage = document.createElement("div")
+        divImage.setAttribute("class", "image")
+        let image = document.createElement("img")
+        image.setAttribute("src", "https://s2.bukalapak.com/img/2021773/large/image-52045417-480x272.jpg")
+        divImage.appendChild(image)
+        alpha.appendChild(divImage)
+        let divExtra = document.createElement("div")
+        divExtra.setAttribute("class", "extra")
+        divExtra.appendChild(document.createTextNode('Description'))
+        alpha.appendChild(divExtra)
+        console.log(alpha)
+        document.getElementById("selfrecomend").appendChild(alpha)
+
+      })
+
+      // document.getElementById("selfrecomend").append(JSON.stringify(data.hasil))
+    })
+  }
 }
 document.addEventListener('DOMContentLoaded',function () {
   //Analize -- baru maintopic
-  app.init();
+  // app.init();
   document.getElementById("analize").addEventListener("click", app.onAnalize);
+  document.getElementById("selfRecomendation").addEventListener("click", app.addRecomend);
 
   //Rekomendasi--- kirim data result ke server, dapetin barang rekomendasinya
   //document.getElementById("recomendation").addEventListener("click"................
